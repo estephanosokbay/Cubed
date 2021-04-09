2ea926d9d914e91e970997bbbf6ceae3bd49bc32
estephanosokbay <82204558+estephanosokbay@users.noreply.github.com>
2021-04-09 15:50:42 +0800

    Add files via upload

---
+++ MyNumber.java
@@ -0,0 +1,21 @@
+public class MyNumber {
+    private int number;
+    
+public MyNumber(int number) {
+    ths.number=number;
+}
+
+public boolean isPrime(){
+    return false;
+    }
+}
+
+public static void main(String[] args) {
+    MyNumber number= mew Number(4);
+    boolean isPrime= number.isPrime();
+    System.out.println("isPrime " + isPrime);
+    
+}
+
+}  
+
